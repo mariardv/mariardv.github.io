daecdf57c5c667c92d568ebcd22a7bd3b6c0e89e
mariardv <maria.rovatidevita@gmail.com>
2019-06-09 18:32:50 +0200

    project upload

---
+++ precache-manifest.98c10706175ada00d3d7a1dcfb2513af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abbd21f1bee89eb14c05",
+    "url": "/mariardv.github.io/static/js/runtime~main.abbd21f1.js"
+  },
+  {
+    "revision": "be3fa89779fc257cb035",
+    "url": "/mariardv.github.io/static/js/main.be3fa897.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/mariardv.github.io/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "be3fa89779fc257cb035",
+    "url": "/mariardv.github.io/static/css/main.f1cd97e5.chunk.css"
+  },
+  {
+    "revision": "9d03d383135815ccbc9fe24eead5c8eb",
+    "url": "/mariardv.github.io/index.html"
+  }
+];
